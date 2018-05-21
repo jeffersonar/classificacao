f701f118e178f0fa6145e67ec845c908f05cf4a2
Jefferson Rodrigues <rodrigues.jepherson@gmail.com>
2018-05-21 14:01:29 -0300

    Primeiro Exercicio

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # classificacao
 Aplicação em python  naive byes  algoritmo multinomialNB
+
+SET PROXY IN WINDOWS 
+set http_proxy=http//user:pwd@ip:porta
+set https_proxy=http//user:pwd@ip:porta
+
+DOWNLOAD ON PIP SITE
+python get-pip.py 
+
+APOS PIP INSTALADO INSTALAR MARCHINE LEARNING
+pip install scikit-learn
+
+INSTALAR COMPLEMENTO MACHINE LEARNING
+pip install scipy
+
---
+++ classificacao.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+
+porco1 =  [1,1,0]
+porco2=   [1,1,0]
+porco3=   [1,1,0]
+cachorro1=[1,1,1]
+cachorro2=[0,1,1]
+cachorro3=[0,1,1]
+dados=[porco1,porco2,porco3,cachorro1,cachorro2,cachorro3]
+
+marcacoes=[1,1,1,-1,-1,-1]
+
+misterioso1=[1,1,1]
+misterioso2=[1,0,0]
+misterioso3=[0,0,1]
+teste=[misterioso1,misterioso2,misterioso3]
+
+marcacoes_teste=[-1, 1, -1]
+
+modelo=MultinomialNB()
+modelo.fit(dados,marcacoes)
+resultado= modelo.predict(teste)
+print(resultado)
+
